8e08465442155aa76e957c1d51d9bc4df1f29d3a
gabee1987 <gabee1987@gmail.com>
2017-11-07 11:54:21 +0100

    exercise 3 is done

---
+++ DictionaryCollections/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise 3
+            Hashtable lookup = new Hashtable();
+            lookup["0"] = "Zero";
+            lookup["1"] = "One";
+            lookup["2"] = "Two";
+            lookup["3"] = "Three";
+            lookup["4"] = "Four";
+            lookup["5"] = "Five";
+            lookup["6"] = "Six";
+            lookup["7"] = "Seven";
+            lookup["8"] = "Eight";
+            lookup["9"] = "Nine";
+
+            string ourNumber = "888-555-1212";
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Exercise 2 (Queue)\n");
+            Console.ResetColor();
+            Console.WriteLine(ourNumber + "\n");
+            foreach (char c in ourNumber)
+            {
+                string digit = c.ToString();
+                if(lookup.ContainsKey(digit))
+                {
+                    Console.WriteLine("Key: {0}  Value: {1}", c.ToString(), lookup[digit]);
+                }
+            }
+                    Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 "# 2nd_SI_week_assignments" 
+
+Exercise 1: Create and Use a Queue
+Exercise 2: Create and Use a Stack
+Exercise 3: Create a Lookup Table
+Exercise 4: Create a ListCollection
+Exercise 5: Create a Generic Collection to Store State Data
