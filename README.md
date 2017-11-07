f9e6d0bf243329d4d34c59aa96d582ed28effd5e
gabee1987 <gabee1987@gmail.com>
2017-11-07 11:19:52 +0100

    added gitignore, and 1. 2. exercise is done

---
+++ README.md
@@ -0,0 +1,1 @@
+"# 2nd_SI_week_assignments" 
---
+++ SequentialCollections/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace SequentialCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercise 1
+            Queue queue = new Queue();
+            queue.Enqueue("First");
+            queue.Enqueue("Second");
+            queue.Enqueue("Third");
+            queue.Enqueue("Fourth");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Exercise 2 (Queue)\n");
+            Console.ResetColor();
+            while (queue.Count > 0)
+            {
+                object obj = queue.Dequeue();
+                Console.WriteLine("From Queue: {0}", obj);
+                Console.ReadLine();
+            }
+
+
+            //Exercise 2
+            Stack stack = new Stack();
+            stack.Push("First");
+            stack.Push("Second");
+            stack.Push("Third");
+            stack.Push("Fourth");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Exercise 2 (Stack)\n");
+            Console.ResetColor();
+            while (stack.Count > 0)
+            {
+                object obj = stack.Pop();
+                Console.WriteLine("From Stack: {0}", obj);
+                Console.ReadLine();
+            }
+        }
+    }
+}
