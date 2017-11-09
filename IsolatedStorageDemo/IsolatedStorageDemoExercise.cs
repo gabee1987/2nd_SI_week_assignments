1bae4059e055188f7803f46408a44ce42179de4a
gabee1987 <gabee1987@gmail.com>
2017-11-09 12:57:12 +0100

    advanced task exercise 4 and 5 is done

---
+++ IsolatedStorageDemo/IsolatedStorageDemoExercise.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsolatedStorageDemo
+{
+    class IsolatedStorageDemoExercise
+    {
+        static void Main(string[] args)
+        {
+            // Exercise 4
+            IsolatedStorageFile userStore = IsolatedStorageFile.GetUserStoreForAssembly();
+            IsolatedStorageFileStream userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Create, userStore);
+            string contents = "";
+
+            StreamWriter userWriter = new StreamWriter(userStream);
+            userWriter.WriteLine("User Prefs");
+            userWriter.Close();
+
+            // Exercise 5
+            string[] files = userStore.GetFileNames("UserSettings.set");
+            if (files.Length == 0)
+            {
+                Console.WriteLine("No files were found.");
+            }
+            else
+            {
+                userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Open, userStore);
+                StreamReader userReader = new StreamReader(userStream);
+                contents = userReader.ReadToEnd();
+
+            }
+            Console.Write("\n File content in isolated storage: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(contents);
+            Console.ResetColor();
+            Console.ReadLine();
+        }
+    }
+}
